1c4cc40f230fe74fc3d548f9565fc63304f8c6b3
Rebecca Lynn Scott <MsMadGirl@users.noreply.github.com>
2017-05-25 14:12:33 -0700

    Add files via upload

---
+++ assignment5/display_results.php
@@ -0,0 +1,96 @@
+display_results.php
+
+<?php
+    // get the data from the form
+    $investment = filter_input(INPUT_POST, 'investment', 
+            FILTER_VALIDATE_FLOAT);
+    $interest_rate = filter_input(INPUT_POST, 'interest_rate', 
+            FILTER_VALIDATE_FLOAT);
+    $years = filter_input(INPUT_POST, 'years', 
+            FILTER_VALIDATE_INT);
+    //posts checkbox results
+    $calc_monthly = $_POST['calc_monthly'];
+
+
+    // validate investment
+    if ($investment === FALSE ) {
+        $error_message = 'Investment must be a valid number.'; 
+    } else if ( $investment <= 0 ) {
+        $error_message = 'Investment must be greater than zero.'; 
+    // validate interest rate
+    } else if ( $interest_rate === FALSE )  {
+        $error_message = 'Interest rate must be a valid number.'; 
+    } else if ( $interest_rate <= 0 ) {
+        $error_message = 'Interest rate must be greater than zero.'; 
+    // validate years
+    } else if ( $years === FALSE ) {
+        $error_message = 'Years must be a valid whole number.';
+    } else if ( $years <= 0 ) {
+        $error_message = 'Years must be greater than zero.';
+    } else if ( $years > 30 ) {
+        $error_message = 'Years must be less than 31.';
+    // set error message to empty string if no invalid entries
+    } else {
+        $error_message = ''; 
+    }
+
+    // if an error message exists, go to the index page
+    if ($error_message != '') {
+        include('index.php');
+        exit();
+    }
+
+    // calculate the future value in months
+    if (isset($calc_monthly)){
+        $future_value = $investment;
+        $months = $years * 12;
+        $monthly_rate = $interest_rate / 12;
+        for ($i = 1; $i <= $months; $i++) {
+            $future_value = ($future_value + ($future_value * $monthly_rate *.01));
+        $cm = 'Yes';       
+        }
+        
+    }
+    //calculate the future value in years
+    else {
+        $future_value = $investment;
+        for ($i = 1; $i <= $years; $i++) {
+            $future_value = ($future_value + ($future_value * $interest_rate *.01));
+        $cm = 'No'; 
+
+        }
+    }
+
+    // apply currency and percent formatting
+    $investment_f = '$'.number_format($investment, 2);
+    $yearly_rate_f = $interest_rate.'%';
+    $future_value_f = '$'.number_format($future_value, 2);
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Future Value Calculator</title>
+    <link rel="stylesheet" type="text/css" href="main.css"/>
+</head>
+<body>
+    <main>
+        <h1>Future Value Calculator</h1>
+
+        <label>Investment Amount:</label>
+        <span><?php echo $investment_f; ?></span><br>
+
+        <label>Yearly Interest Rate:</label>
+        <span><?php echo $yearly_rate_f; ?></span><br>
+
+        <label>Number of Years:</label>
+        <span><?php echo $years; ?></span><br>
+
+        <label>Future Value:</label>
+        <span><?php echo $future_value_f; ?></span><br>
+
+        <label>Compound Monthly:</label>
+        <span><?php echo $cm; ?></span>
+    </main>
+</body>
+</html>
